418c475b7c91bd11cbe24636c52ef0cced6ea17c
D-coder <adi_veer@hotmail.com>
2021-05-03 18:15:31 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,10 @@ const About = ({location }) => {
         <Layout location={location} title="About">
             <Seo title="About US" />
             <Bio />
-            <p>This is About Us page</p>
+            <p>An artist, producer, DJ, composer and songwriter based in Delhi and produces Dance-pop, House and RnB music. His music is majorly influenced by pop sounds and melodies as he grew up listening to them.
+He has been doing music since he was like 8, but for him, the career really started when he purchased his first little synth which motivated him to play melodies all day and night.
+
+For him, music is life; the sounds we hear, voices and colors around automatically spark a random melody, beat.</p>
         </Layout>
     )
 }
